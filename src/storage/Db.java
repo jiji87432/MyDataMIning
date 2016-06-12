4edc8e41fcb75412d0e3898c609de3b24f46c941
luoxiao195 <409494308@qq.com>
2016-06-12 12:20:42 +0800

    x

--- src/storage/Db.java
+++ src/storage/Db.java
@@ -85,6 +85,46 @@ public class Db {
 		
 		return id;
 	}
+	//已知名字查找用户
+	public int selectUser(String name){
+		int id=0;
+		String sql="select userId from user where name=?";
+		try {
+			pre=conn.prepareStatement(sql);
+			pre.setString(1, name);
+			rs=pre.executeQuery();
+			if(rs.next()){
+				id=rs.getInt("userId");
+			}
+			else
+				System.out.println("user is not exit");
+				
+		} catch (Exception e) {
+			System.out.println("查询出错");
+			System.out.println(e.getMessage());
+		}
+		
+		return id;
+	}
+	//保存用户
+	public int saveUser(String loc,String prefession,int gen,String name){
+		int id=0;
+		String sql="insert into user (location,profession,gender,name) values (?,?,?,?)";
+		//System.out.println(sql);
+		try {
+			pre=conn.prepareStatement(sql);
+			pre.setString(1, loc);
+			pre.setString(2, prefession);
+			pre.setInt(3, gen);
+			pre.setString(4, name);
+			id=pre.executeUpdate();
+		} catch (Exception e) {
+			System.out.println("保存用户出错");
+			System.out.println(e.getMessage());
+		}
+		
+		return id;
+	}
 	//关闭链接
 	public static void closeConn() throws SQLException{
 		try {
